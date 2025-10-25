9b1800dcd52ece446a93d6b9dd30e7098ad9d3f5
Akcarc <babluverma636813@gmail.com>
2025-10-25 18:18:50 +0530

    sreer change

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 const eventRoutes = require('./routes/events');
 const authRoutes = require('./routes/auth');
@@ -7,15 +8,16 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  next();
-});
+
+// Use CORS middleware
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'],
+  methods: ['GET','POST','PATCH','DELETE'],
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials: true // if you send cookies/auth headers
+}));
 
 app.use(authRoutes);
-
 app.use('/events', eventRoutes);
 
 app.use((error, req, res, next) => {
@@ -24,4 +26,6 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message });
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server running');
+});
